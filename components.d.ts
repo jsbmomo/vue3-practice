42e75974a481e2e5d48a82314e602b633578c453
jsbmomo <tytyes40@gmail.com>
2026-01-13 11:20:30 +0900

    :sparkles: 자동완성 인터페이스 정의

---
+++ components.d.ts
@@ -0,0 +1,8 @@
+// Global 컴포넌트에 인터페이스를 정의.
+// RouterView, RouterLink 태그가 등록 및 자동 완성되도록 설정
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    RouterLink: typeof import('vue-router') ['RouterLink']
+    RouterView: typeof import('vue-router') ['RouterView']
+  }
+}
